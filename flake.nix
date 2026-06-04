5a6db79f77ae55266e51fc0ccff4c621cc8d83b3
xb-bx <nazarnik050406@gmail.com>
2026-06-04 15:30:51 +0300

    fix

--- flake.nix
+++ flake.nix
@@ -4,13 +4,12 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
-    lib.mkDotnetApp = { projectName, projectVersion, src, dotnetSdk }: 
+    lib.mkDotnetApp = { projectName, projectVersion, src, dotnetSdk, nugetHash }: 
       let
         system = "x86_64-linux";
         pkgs = nixpkgs.legacyPackages.${system};
         csprojFile = "./${projectName}.csproj";
 
-        # The generic, self-contained cache block using the passed SDK
         nugetCache = pkgs.stdenv.mkDerivation {
           name = "${pkgs.lib.strings.toLower projectName}-nuget-cache";
           inherit src; 
@@ -18,7 +17,9 @@
           nativeBuildInputs = [ dotnetSdk ];
           outputHashAlgo = "sha256";
           outputHashMode = "recursive";
-          outputHash = pkgs.lib.fakeHash; 
+          
+          # Directly using the passed argument here
+          outputHash = nugetHash; 
 
           buildPhase = ''
             export HOME=$TMPDIR
@@ -33,9 +34,6 @@
         pname = pkgs.lib.strings.toLower projectName;
         version = projectVersion;
         inherit src;
-
-        # Expose the specific cache block so it can be overridden downstream
-        passthru = { inherit nugetCache; };
 
         nativeBuildInputs = [ dotnetSdk pkgs.git ];
 
